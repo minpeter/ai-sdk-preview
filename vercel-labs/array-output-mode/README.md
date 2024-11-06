92814e82aeff08680bfa6262616adf677aa82b48
minpeter <minpeter@friendli.ai>
2024-11-06 15:03:01 +0900

    Included in monorepo after copying as is

---
+++ vercel-labs/array-output-mode/README.md
@@ -0,0 +1,47 @@
+# Structured Object Generation Array Mode
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fvercel-labs%2Fstructured-object-generation-array-mode&env=OPENAI_API_KEY)
+
+This is a Next.js project that uses the the [`streamObject`](https://sdk.vercel.ai/docs/reference/ai-sdk-core/stream-object) function from the Vercel AI SDK to stream structured objects. This project showcases the new [output mode](https://sdk.vercel.ai/docs/ai-sdk-core/generating-structured-data#output-strategy-array) which allows you to stream structured objects by complete object rather than by token. This solves layout shift issues common to AI applications using structured object generation.
+
+## Getting Started
+
+First, clone the repository:
+
+```bash
+git clone https://github.com/vercel-labs/structured-object-generation-array-mode.git
+```
+
+Then, install the dependencies:
+
+```bash
+pnpm install
+```
+
+Copy the .env.exampmle file to .env.local and fill in the required environment variables:
+
+```bash
+OPENAI_API_KEY=sk-...
+```
+
+Finally, run the development server:
+
+```bash
+pnpm dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+## Learn More
+
+To learn more about Next.js, take a look at the following resources:
+
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+- [Vercel AI SDK](https://sdk.vercel.ai/docs/) - learn about the Vercel AI SDK.
+
+## Deploy on Vercel
+
+The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+
+Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
---
+++ vercel-labs/array-output-mode/components/icons.tsx
@@ -0,0 +1,131 @@
+export const VercelIcon = ({ size = 17 }) => {
+  return (
+    <svg
+      height={size}
+      strokeLinejoin="round"
+      viewBox="0 0 16 16"
+      width={size}
+      style={{ color: "currentcolor" }}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M8 1L16 15H0L8 1Z"
+        fill="currentColor"
+      ></path>
+    </svg>
+  );
+};
+
+export const InformationIcon = ({ size = 17 }) => {
+  return (
+    <svg
+      height={size}
+      strokeLinejoin="round"
+      viewBox="0 0 16 16"
+      width={size}
+      style={{ color: "currentcolor" }}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M8 14.5C11.5899 14.5 14.5 11.5899 14.5 8C14.5 4.41015 11.5899 1.5 8 1.5C4.41015 1.5 1.5 4.41015 1.5 8C1.5 11.5899 4.41015 14.5 8 14.5ZM8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM6.25 7H7H7.74999C8.30227 7 8.74999 7.44772 8.74999 8V11.5V12.25H7.24999V11.5V8.5H7H6.25V7ZM8 6C8.55229 6 9 5.55228 9 5C9 4.44772 8.55229 4 8 4C7.44772 4 7 4.44772 7 5C7 5.55228 7.44772 6 8 6Z"
+        fill="currentColor"
+      ></path>
+    </svg>
+  );
+};
+
+export const LoadingIcon = () => {
+  return (
+    <svg
+      height="16"
+      strokeLinejoin="round"
+      viewBox="0 0 16 16"
+      width="16"
+      style={{ color: "currentcolor" }}
+    >
+      <g clipPath="url(#clip0_2393_1490)">
+        <path d="M8 0V4" stroke="currentColor" strokeWidth="1.5"></path>
+        <path
+          opacity="0.5"
+          d="M8 16V12"
+          stroke="currentColor"
+          strokeWidth="1.5"
+        ></path>
+        <path
+          opacity="0.9"
+          d="M3.29773 1.52783L5.64887 4.7639"
+          stroke="currentColor"
+          strokeWidth="1.5"
+        ></path>
+        <path
+          opacity="0.1"
+          d="M12.7023 1.52783L10.3511 4.7639"
+          stroke="currentColor"
+          strokeWidth="1.5"
+        ></path>
+        <path
+          opacity="0.4"
+          d="M12.7023 14.472L10.3511 11.236"
+          stroke="currentColor"
+          strokeWidth="1.5"
+        ></path>
+        <path
+          opacity="0.6"
+          d="M3.29773 14.472L5.64887 11.236"
+          stroke="currentColor"
+          strokeWidth="1.5"
+        ></path>
+        <path
+          opacity="0.2"
+          d="M15.6085 5.52783L11.8043 6.7639"
+          stroke="currentColor"
+          strokeWidth="1.5"
+        ></path>
+        <path
+          opacity="0.7"
+          d="M0.391602 10.472L4.19583 9.23598"
+          stroke="currentColor"
+          strokeWidth="1.5"
+        ></path>
+        <path
+          opacity="0.3"
+          d="M15.6085 10.4722L11.8043 9.2361"
+          stroke="currentColor"
+          strokeWidth="1.5"
+        ></path>
+        <path
+          opacity="0.8"
+          d="M0.391602 5.52783L4.19583 6.7639"
+          stroke="currentColor"
+          strokeWidth="1.5"
+        ></path>
+      </g>
+      <defs>
+        <clipPath id="clip0_2393_1490">
+          <rect width="16" height="16" fill="white"></rect>
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export const MasonryIcon = () => {
+  return (
+    <svg
+      height="16"
+      strokeLinejoin="round"
+      viewBox="0 0 16 16"
+      width="16"
+      style={{ color: "currentcolor" }}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M2.5 6.5V2.5H5.5V6.5H2.5ZM1 2C1 1.44772 1.44772 1 2 1H6C6.55228 1 7 1.44772 7 2V7C7 7.55228 6.55228 8 6 8H2C1.44772 8 1 7.55228 1 7V2ZM2.5 13.5V11.5H5.5V13.5H2.5ZM1 11C1 10.4477 1.44772 10 2 10H6C6.55228 10 7 10.4477 7 11V14C7 14.5523 6.55228 15 6 15H2C1.44772 15 1 14.5523 1 14V11ZM10.5 2.5V4.5H13.5V2.5H10.5ZM10 1C9.44772 1 9 1.44772 9 2V5C9 5.55228 9.44772 6 10 6H14C14.5523 6 15 5.55228 15 5V2C15 1.44772 14.5523 1 14 1H10ZM13.5 13.5H10.5V9.5H13.5V13.5ZM9 9C9 8.44772 9.44772 8 10 8H14C14.5523 8 15 8.44772 15 9V14C15 14.5523 14.5523 15 14 15H10C9.44772 15 9 14.5523 9 14V9Z"
+        fill="currentColor"
+      ></path>
+    </svg>
+  );
+};
